36ea2d01699a212027dd956183abd9dba65a924f
Tkakishak <tkakishakd@mail.ru>
2023-03-16 00:16:26 +0300

    HW0315

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
+    public static void checkYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = 2023;
+        checkYear(year);
     }
 }
+
+
+
